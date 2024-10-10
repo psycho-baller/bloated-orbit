1b08697789025ca7314223849fd2922d9f12cfc8
Rami Maalouf <rami.rami@ucalgary.ca>
2024-10-10 11:05:44 -0600

    added useful logs

--- Orbit/UI/ViewModels/UserViewModel.swift
+++ Orbit/UI/ViewModels/UserViewModel.swift
@@ -35,32 +35,55 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
 
     @MainActor
     func initialize() async {
+        print(
+            "UserViewModel - initialize: Initializing user list and subscribing to real-time updates."
+        )
         await listUsers()
         await subscribeToRealtimeUpdates()
-
     }
 
     @MainActor
-    func createUser(userData: UserModel) async {
+    func createUser(userData: UserModel) async -> UserDocument? {
         do {
+            print(
+                "UserViewModel - createUser: Attempting to create user with ID \(userData.accountId)."
+            )
             let newUser = try await userManagementService.createUser(userData)
-            print("User created: \(newUser)")
+            print(
+                "UserViewModel - createUser: User \(newUser.id) successfully created."
+            )
             await listUsers()  // Refresh the user list after creation
+            return newUser
         } catch {
-            print("Source: createUser - \(error.localizedDescription)")
+            print(
+                "UserViewModel - Source: createUser - Error while creating user with ID \(userData.accountId): \(error.localizedDescription)"
+            )
             self.error = error.localizedDescription
         }
+        return nil
     }
 
     @MainActor
     func updateUser(id: String, updatedUser: UserModel) async {
         do {
-            let updatedUserDocument =
-                try await userManagementService.updateUser(
-                    accountId: id, updatedUser: updatedUser)
+            print(
+                "UserViewModel - updateUser: Attempting to update user with ID \(id)."
+            )
+            guard
+                let updatedUserDocument =
+                    try await userManagementService.updateUser(
+                        accountId: id, updatedUser: updatedUser)
+            else {
+                throw NSError(domain: "User not found", code: 404, userInfo: nil)
+            }
+            print(
+                "UserViewModel - updateUser: User \(updatedUserDocument?.id) successfully updated."
+            )
             await listUsers()  // Refresh the user list after update
         } catch {
-            print("Source: updateUser - \(error.localizedDescription)")
+            print(
+                "UserViewModel - Source: updateUser - Error while updating user with ID \(id): \(error.localizedDescription)"
+            )
             self.error = error.localizedDescription
         }
     }
@@ -68,12 +91,18 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
     @MainActor
     func deleteUser(id: String) async {
         do {
+            print(
+                "UserViewModel - deleteUser: Attempting to delete user with ID \(id)."
+            )
             try await userManagementService.deleteUser(id)
-            print("User deleted: \(id)")
+            print(
+                "UserViewModel - deleteUser: User \(id) successfully deleted.")
             await listUsers()  // Refresh the user list after deletion
         } catch {
+            print(
+                "UserViewModel - Source: deleteUser - Error while deleting user with ID \(id): \(error.localizedDescription)"
+            )
             self.error = error.localizedDescription
-            print("Source: deleteUser - \(error.localizedDescription)")
         }
     }
 
@@ -113,20 +142,26 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
 
     @MainActor
     func listUsers(queries: [String]? = nil) async {
+        print("UserViewModel - listUsers: Fetching user list.")
         isLoading = true
         do {
             let userDocuments = try await userManagementService.listUsers(
                 queries: queries)
             self.users = userDocuments.map { $0.data }
+            print(
+                "UserViewModel - listUsers: Successfully fetched \(self.users.count) users."
+            )
         } catch {
+            print(
+                "UserViewModel - Source: listUsers - Error: \(error.localizedDescription)"
+            )
             self.error = error.localizedDescription
-            print("Source: listUsers - \(error.localizedDescription)")
         }
         isLoading = false
     }
 
-    // Function to handle interest selection
     func toggleInterest(_ interest: String) {
+        print("UserViewModel - toggleInterest: Toggling interest: \(interest).")
         if let index = selectedInterests.firstIndex(of: interest) {
             selectedInterests.remove(at: index)
         } else {
@@ -134,9 +169,11 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
         }
     }
 
-    // MARK - Location
-    // LocationManagerDelegate method
+    // MARK - Location Updates
     func didUpdateLocation(latitude: Double, longitude: Double) {
+        print(
+            "UserViewModel - didUpdateLocation: Received location update - Latitude: \(latitude), Longitude: \(longitude)."
+        )
         self.currentLocation = CLLocationCoordinate2D(
             latitude: latitude, longitude: longitude)
         Task {
@@ -145,16 +182,19 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
         }
     }
 
-    // Update current user's location in the database
     @MainActor
     func updateCurrentUserLocation(latitude: Double, longitude: Double) async {
-
+        print(
+            "UserViewModel - updateCurrentUserLocation: Attempting to update current user's location - Latitude: \(latitude), Longitude: \(longitude)."
+        )
         do {
             guard
                 let currentUser =
                     try await userManagementService.getCurrentUser()
             else {
-                print("Current user not found. Anonymous login?")
+                print(
+                    "UserViewModel - updateCurrentUserLocation: Current user not found, possibly anonymous."
+                )
                 return
             }
             var updatedUser = currentUser
@@ -163,34 +203,49 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
 
             await updateUser(
                 id: currentUser.accountId, updatedUser: updatedUser)
+            print(
+                "UserViewModel - updateCurrentUserLocation: Successfully updated location for user \(currentUser.accountId)."
+            )
         } catch {
-            print("Source: updateCurrentUserLocation - \(error.localizedDescription)")
+            print(
+                "UserViewModel - Source: updateCurrentUserLocation - Error: \(error.localizedDescription)"
+            )
             self.error = error.localizedDescription
         }
     }
 
     @MainActor
     func subscribeToRealtimeUpdates() async {
+        print(
+            "UserViewModel - subscribeToRealtimeUpdates: Subscribing to real-time updates."
+        )
         do {
             let subscription = try await appwriteRealtimeClient.subscribe(
                 channels: [
                     "databases.\(AppwriteService.shared.databaseId).collections.users.documents"
-                ]) { event in
-                    if let payload = event.payload {
-                        Task {
-                            let updatedUser = try JSONDecoder().decode(
-                                UserModel.self,
-                                from: JSONSerialization.data(
-                                    withJSONObject: payload))
-                            self.handleRealtimeUserUpdate(updatedUser)
-                        }
+                ]
+            ) { event in
+                if let payload = event.payload {
+                    Task {
+                        let updatedUser = try JSONDecoder().decode(
+                            UserModel.self,
+                            from: JSONSerialization.data(
+                                withJSONObject: payload))
+                        self.handleRealtimeUserUpdate(updatedUser)
+                        print(
+                            "UserViewModel - subscribeToRealtimeUpdates: Received real-time update for user \(updatedUser.accountId)."
+                        )
                     }
                 }
-        } catch {
-            self.error = error.localizedDescription
+            }
             print(
-                "Error decoding user data: \(error.localizedDescription)"
+                "UserViewModel - subscribeToRealtimeUpdates: Successfully subscribed to real-time updates."
             )
+        } catch {
+            print(
+                "UserViewModel - Source: subscribeToRealtimeUpdates - Error: \(error.localizedDescription)"
+            )
+            self.error = error.localizedDescription
         }
     }
 
@@ -198,22 +253,35 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
     func handleRealtimeUserUpdate(_ updatedUser: UserModel) {
         if let index = users.firstIndex(where: { $0.id == updatedUser.id }) {
             users[index] = updatedUser
+            print(
+                "UserViewModel - handleRealtimeUserUpdate: Updated user \(updatedUser.accountId) in local list."
+            )
         } else {
             users.append(updatedUser)
+            print(
+                "UserViewModel - handleRealtimeUserUpdate: Added new user \(updatedUser.accountId) to local list."
+            )
         }
     }
 
     // Helper function to filter users by location proximity
     func usersNearby(users: [UserModel], radius: Double = 10000) -> [UserModel] {
         guard let currentLocation = currentLocation else {
-            print("user location not available")
+            print(
+                "UserViewModel - usersNearby: Current location not available.")
             return []
         }
-        print("currentUserLocation: \(currentLocation)")
+        print(
+            "UserViewModel - usersNearby: Filtering users within \(radius) meters of current location."
+        )
         return users.filter { user in
             guard let userLat = user.latitude, let userLong = user.longitude
-            else { return false }
-            print("userLat: \(userLat), userLong: \(userLong)")
+            else {
+                print(
+                    "UserViewModel - usersNearby: Skipping user \(user.id), missing location data."
+                )
+                return false
+            }
             let userLocation = CLLocation(
                 latitude: Double(userLat), longitude: Double(userLong))
             let currentCLLocation = CLLocation(
@@ -221,7 +289,9 @@ class UserViewModel: NSObject, ObservableObject, LocationManagerDelegate {
                 longitude: currentLocation.longitude)
             let distanceFromEachOther = currentCLLocation.distance(
                 from: userLocation)
-            print("distance: \(distanceFromEachOther)")
+            print(
+                "UserViewModel - usersNearby: User \(user.id) is \(distanceFromEachOther) meters away."
+            )
             return distanceFromEachOther <= radius
         }
     }
